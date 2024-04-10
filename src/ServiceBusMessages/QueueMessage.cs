f508f0a9ad153d70e389a5f7596800d8ad791e10
Ross Boe <Ross.Boe@trunorthdynamics.com>
2024-04-10 10:38:33 -0400

    service bus queue function

---
+++ src/ServiceBusMessages/QueueMessage.cs
@@ -0,0 +1,118 @@
+using System;
+using System.Linq;
+using System.Runtime.Serialization.Json;
+using System.Threading.Tasks;
+using Azure.Messaging.ServiceBus;
+using DataverseAzureFunctionsCommon;
+using DataverseAzureFunctionsCommon.Dataverse.Model;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Azure.WebJobs.ServiceBus;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Microsoft.PowerPlatform.Dataverse.Client;
+using Microsoft.Xrm.Sdk;
+
+namespace ServiceBusMessages
+{
+    public class QueueMessage
+    {
+        private readonly ServiceClient _serviceClient;
+        private readonly IConfiguration _configuration;
+
+        public QueueMessage(IOrganizationService serviceClient, IConfiguration config)
+        {
+            _serviceClient = (ServiceClient)serviceClient;
+            _configuration = config;
+        }
+
+        [FunctionName("QueueMessage")]
+        public async Task Run([ServiceBusTrigger("%DataverseQueue%", Connection = "ServiceBusQueueNameSpace")]
+                ServiceBusReceivedMessage message,
+                ServiceBusMessageActions messageActions,
+                ExecutionContext executionContext,
+                ILogger log)
+        {
+            try
+            {
+                log.LogInformation($"InvocationId: {executionContext.InvocationId} QueueMessage: {message.Body}");
+                //Validate the message is from the expected Dataverse Organization
+                if (message.ApplicationProperties.ContainsKey("http://schemas.microsoft.com/xrm/2011/Claims/Organization"))
+                {
+                    var orgValue = (string)message.ApplicationProperties["http://schemas.microsoft.com/xrm/2011/Claims/Organization"];
+                    log.LogInformation($"Organization: {orgValue}");
+                    if (orgValue.Contains(_configuration.GetValue<string>("expectedOrg")))
+                    {
+                        log.LogInformation("Expected Org");
+                    }
+                    else
+                    {
+                        log.LogWarning($"Mesage org not expected: {orgValue}");
+                        await messageActions.DeadLetterMessageAsync(message);
+                    }
+                }
+                else
+                {
+                    log.LogWarning("Missing Org Header Value");
+                    await messageActions.DeadLetterMessageAsync(message);
+                }
+                if (message.ApplicationProperties.ContainsKey("http://schemas.microsoft.com/xrm/2011/Claims/User"))
+                    log.LogInformation($"User: {(string)message.ApplicationProperties["http://schemas.microsoft.com/xrm/2011/Claims/User"]}");
+
+                if (message.ApplicationProperties.ContainsKey("http://schemas.microsoft.com/xrm/2011/Claims/InitiatingUser"))
+                    log.LogInformation($"InitiatingUser: {(string)message.ApplicationProperties["http://schemas.microsoft.com/xrm/2011/Claims/InitiatingUser"]}");
+
+                if (message.ApplicationProperties.ContainsKey("http://schemas.microsoft.com/xrm/2011/Claims/EntityLogicalName"))
+                    log.LogInformation($"EntityLogicalName: {(string)message.ApplicationProperties["http://schemas.microsoft.com/xrm/2011/Claims/EntityLogicalName"]}");
+
+                if (message.ApplicationProperties.ContainsKey("http://schemas.microsoft.com/xrm/2011/Claims/RequestName"))
+                    log.LogInformation($"RequestName: {(string)message.ApplicationProperties["http://schemas.microsoft.com/xrm/2011/Claims/RequestName"]}");
+
+                if (message.ApplicationProperties.ContainsKey("http://schemas.microsoft.com/xrm/2011/Claims/InitiatingUserAgent"))
+                    log.LogInformation($"InitiatingUserAgent: {(string)message.ApplicationProperties["http://schemas.microsoft.com/xrm/2011/Claims/InitiatingUserAgent"]}");
+
+                var messageDataExeeded = false;
+                if (message.ApplicationProperties.Keys.Any(ap => ap.Contains("MessageMaxSizeExceeded ")))
+                {
+                    log.LogWarning("MessageMaxSizeExceeded");
+                    messageDataExeeded = true;
+                }
+
+                var remoteContext = RemoteContextDeserializer.DeserializeJsonString<RemoteExecutionContext>(message.Body.ToString());
+
+                RjB_DemoAzureFunctionLog demoLog;
+                //Check if the size was exceeded. If not retrieve the entity from the Target otherwise retrieve the row
+                if (!messageDataExeeded)
+                {
+                    demoLog = ((Entity)remoteContext.InputParameters["Target"]).ToEntity<RjB_DemoAzureFunctionLog>();
+                }
+                else
+                {
+                    demoLog = (await _serviceClient.RetrieveAsync(RjB_DemoAzureFunctionLog.EntityLogicalName, remoteContext.PrimaryEntityId, new Microsoft.Xrm.Sdk.Query.ColumnSet(
+                        RjB_DemoAzureFunctionLog.Fields.RjB_AzureFunctionWriteBack, RjB_DemoAzureFunctionLog.Fields.RjB_Type
+                        ))).ToEntity<RjB_DemoAzureFunctionLog>();
+                }
+
+                log.LogInformation($"Primary Entity {demoLog.LogicalName} {demoLog.RjB_Name} {demoLog.Id}");
+                //write back a date/time just to prove the function was triggered
+                if (demoLog.RjB_Type == RjB_TypeOfAzureFunction.Servicebus)
+                {
+                    var addNote = new Annotation();
+                    addNote.NoteText = $"Service Bus Azure Function Writing Back to the log record";
+                    addNote.ObjectId = new EntityReference(RjB_DemoAzureFunctionLog.EntityLogicalName, remoteContext.PrimaryEntityId);
+                    await _serviceClient.CreateAsync(addNote);
+                    log.LogInformation("WriteBack Successful");
+                }
+
+                await messageActions.CompleteMessageAsync(message);
+            }
+            catch (Exception ex)
+            {
+                log.LogError(ex, $"Error Occured {executionContext.InvocationId}");
+                await messageActions.AbandonMessageAsync(message);
+                throw;
+            }
+            
+        }
+    }
+}
---
+++ src/ServiceBusMessages/Startup.cs
@@ -0,0 +1,59 @@
+﻿using Azure.Core;
+using Azure.Identity;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.PowerPlatform.Dataverse.Client;
+using Microsoft.Xrm.Sdk;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+[assembly: FunctionsStartup(typeof(ServiceBusMessages.Startup))]
+namespace ServiceBusMessages
+{
+    
+    internal class Startup : FunctionsStartup
+    {
+        private static string dataverseurl;
+        public override void ConfigureAppConfiguration(IFunctionsConfigurationBuilder builder)
+        {
+            builder.ConfigurationBuilder.AddUserSecrets(Assembly.GetExecutingAssembly());
+            var config = builder.ConfigurationBuilder.Build();
+            dataverseurl = config["dataverseurl"];
+            base.ConfigureAppConfiguration(builder);
+        }
+
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            var x = builder.GetContext();
+            builder.Services.AddMemoryCache();
+            builder.Services.AddSingleton(new DefaultAzureCredential());
+            builder.Services.AddSingleton<IOrganizationService, ServiceClient>(provider =>
+            {
+                var managedIdentity = provider.GetRequiredService<DefaultAzureCredential>();
+                var environment = dataverseurl;
+                var cache = provider.GetService<IMemoryCache>();
+                return new ServiceClient(
+                        tokenProviderFunction: f => GetDataverseToken(environment, managedIdentity, cache),
+                        instanceUrl: new Uri(environment),
+                        useUniqueInstance: true);
+            });
+        }
+
+        private async Task<string> GetDataverseToken(string environment, DefaultAzureCredential credential, IMemoryCache cache)
+        {
+            var accessToken = await cache.GetOrCreateAsync(environment, async (cacheEntry) => {
+                cacheEntry.AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(20);
+                var token = (await credential.GetTokenAsync(new TokenRequestContext(new[] { $"{environment}/.default" })));
+                return token;
+            });
+            return accessToken.Token;
+        }
+    }
+}
